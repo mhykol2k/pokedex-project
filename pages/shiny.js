f619248293ae8516d0ccfd8316bf34c7fe60412b
mm1089 <michael@mclain.co.uk>
2022-04-22 20:03:44 +0100

    Initial Commit, Shiny dex, Normal dex.

--- pages/shiny.js
+++ pages/shiny.js
@@ -18,11 +18,11 @@ export default function Home({ pokemonData }) {
         <title>NextJS Pokédex</title>
       </Head>
 
-      <main className='bg-gradient-to-t from-indigo-200 to-pink-200 min-h-screen px-4 pt-12 pb-4'>
+      <main className='bg-gradient-to-t from-yellow-300 to-yellow-200 via-yellow-100 min-h-screen px-4 pt-12 pb-4'>
         <div className='sm:flex sm:justify-center lg:flex lg:justify-center mt-2 block'>
           <div className='flex justify-between items-center pl-1 pr-3 mx-4 py-1 border-b border-gray-600'>
             <input
-              className='bg-transparant outline-none font-thin text-gray-600'
+              className='bg-transparent outline-none font-thin text-gray-600'
               placeholder='Search for a Pokémon'
               type='text'
               onChange={(e) => search(e.target.value)}
@@ -38,11 +38,11 @@ export default function Home({ pokemonData }) {
               const pokemon_id = Pokemon.url.split("/")[6];
               const padded_id = pokemon_id.padStart(1, "0");
               return(
-                <div className='w-24 md:w-28 lg:w-32 xl:w-32 bg-gradient-to-t from-blue-200 to-pink-100  p-3 m-1 sm:m-2 border-gray-300 border-2 hover:border-indigo-400 rounded-xl text-center cursor-pointer shadow-sm hover:shadow-lg hover:opacity-90 transition-all duration-200'>
+                <div className='bg-gradient-to-t from-yellow-400 to-yellow-200 p-3 m-1 sm:m-2 border-gray-300 border-2 hover:border-indigo-400 rounded-xl text-center cursor-pointer shadow-sm hover:shadow-lg hover:opacity-90 transition-all duration-200'>
                   <h3 className='capitalize text-gray-500 text-center w-full lg:text-xs text-sm'> #{pokemon_id}</h3>
                 <div>
                   <img
-                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${padded_id}.png`}
+                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${padded_id}.png`}
                     className='w-full h-full bg-gradient-to-tl shadow-inner rounded-full hover:scale-105 selection:border-red-500 transition ease-in '
                     type=''
                     loading='lazy'
